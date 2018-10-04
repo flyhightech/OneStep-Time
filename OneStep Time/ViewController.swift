ecabea584416fe52d255d796bc73d77999232b50
Bernard Huff <bernard.huff008@gmail.com>
2018-10-04 12:13:55 -0500

    Initial Commit

---
+++ OneStep Time/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  OneStep Time
+//
+//  Created by Bernard Huff on 10/4/18.
+//  Copyright © 2018 Flyhightech.LLC. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
